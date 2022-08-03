e47f4f7bfd14d697c16b3c2b0bc0b2ba40743f03
Emil Məmmədov <emill.memmedovv@gmail.com>
2022-08-03 20:08:16 +0400

    dockerize

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,21 @@
 import { Module } from '@nestjs/common';
+import {ConfigModule} from "@nestjs/config";
+import * as Joi from "joi";
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      validationSchema: Joi.object({
+        DB_USERNAME: Joi.string().required(),
+        DB_PASSWORD: Joi.string().required(),
+        DB_DATABASE: Joi.string().required(),
+        AWS_REGION: Joi.string().required(),
+        AWS_ACCESS_KEY_ID: Joi.string().required(),
+        AWS_SECRET_ACCESS_KEY: Joi.string().required(),
+        PORT: Joi.number(),
+        PROJECT_NAME: Joi.string().required()
+      })
+    })
+  ],
 })
 export class AppModule {}
